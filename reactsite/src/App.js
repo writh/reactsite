735381c456a31452449e3cbd84690b3fad507227
Cliff Christensen <Cliff@HOMEs-MacBook-Pro.local>
2019-06-07 14:23:37 -0600

    created react app

---
+++ reactsite/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <Home></Home>
+  );
+}
+
+export default App;
---
+++ reactsite/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Home() {
+  return (
+    <div>
+      <p>
+        "hi"
+      </p>
+    </div>
+  );
+}
+
+export default Home;
